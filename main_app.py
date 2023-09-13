c9655a7b4036aa4f11a8a370337af7e2c351f77e
Deepak Sharma <deepak.sharma@innovaccer.com>
2023-09-13 15:41:15 +0530

    added main app

---
+++ main_app.py
@@ -0,0 +1,299 @@
+import dask
+import whisperx
+import os
+
+# from dotenv import load_dotenv
+import torchaudio
+from fastapi import FastAPI, File, UploadFile
+import time
+import torch
+from typing import Optional
+
+
+import nemo.collections.asr as nemo_asr
+from pydub import AudioSegment
+
+# load_dotenv()
+
+app = FastAPI()
+
+if torch.cuda.is_available():
+    device = "cuda"
+    batch_size = 32
+    compute_type = "float16"  #
+else:
+    device = "cuda"
+    batch_size = 32
+    compute_type = "float16"
+
+
+model_name_en = "WAV2VEC2_ASR_BASE_960H"
+model_name_es = "VOXPOPULI_ASR_BASE_10K_ES"
+
+pipeline_type = "torchaudio"
+
+bundle_en = torchaudio.pipelines.__dict__[model_name_en]
+align_model_en = bundle_en.get_model().to(device)
+labels_en = bundle_en.get_labels()
+align_dictionary_en = {c.lower(): i for i, c in enumerate(labels_en)}
+align_metadata_en = {
+    "language": "en",
+    "dictionary": align_dictionary_en,
+    "type": pipeline_type,
+}
+
+
+bundle_es = torchaudio.pipelines.__dict__[model_name_es]
+align_model_es = bundle_es.get_model().to(device)
+labels_es = bundle_es.get_labels()
+align_dictionary_es = {c.lower(): i for i, c in enumerate(labels_es)}
+align_metadata_es = {
+    "language": "es",
+    "dictionary": align_dictionary_es,
+    "type": pipeline_type,
+}
+
+speaker_model = nemo_asr.models.EncDecSpeakerLabelModel.from_pretrained(
+    "nvidia/speakerverification_en_titanet_large"
+)
+
+
+def is_identical_speaker(audio_1, audio_2, threshold):
+    return speaker_model.verify_speakers(audio_1, audio_2, threshold)
+
+
+def cut_wav(input_wav, start_time, end_time, output_wav):
+    audio = AudioSegment.from_wav(input_wav)
+    cut_audio = audio[start_time:end_time]
+    cut_audio.export(output_wav, format="wav")
+
+
+model = whisperx.load_model(
+    "large-v2", device, compute_type=compute_type, asr_options={"beam_size": 1}
+)
+
+local_cache = {}
+
+
+@app.get("/end_transcription")
+async def get_request(unique_key: str):
+    start_time = time.time()
+    existing_speakers = local_cache.pop(unique_key, {}).get("existing_speakers")
+    print(existing_speakers)
+    if existing_speakers:
+        for speaker in existing_speakers:
+            print("PATH EXISTS", os.path.exists(speaker.get("speaker_audio_file")))
+            if os.path.exists(speaker.get("speaker_audio_file")):
+                os.remove(speaker.get("speaker_audio_file"))
+    # local_cache.pop(unique_key, None)
+    return {"message": f"data for {unique_key} deleted from local cache"}
+
+
+diarize_model = whisperx.DiarizationPipeline(device=device, use_auth_token="y")
+
+
+@app.post("/transcribe")
+async def transcribe(
+    audio_file: UploadFile = File(...),
+    unique_key: Optional[str] = None,
+    diarize: Optional[bool] = False,
+    threshold: float = 0.7,
+):
+    start_time = time.time()
+    if local_cache.get(unique_key):
+        pass
+    else:
+        local_cache[unique_key] = {}
+        local_cache[unique_key]["existing_speakers"] = []
+        local_cache[unique_key]["unique_speakers"] = set()
+
+    audio_path = f"{audio_file.filename}"
+    with open(audio_path, "wb") as f:
+        f.write(await audio_file.read())
+
+    audio = whisperx.load_audio(audio_path)
+
+    def diarize_audio():
+        t2 = time.time()
+        diarize_segments = diarize_model(audio)
+        t3 = time.time()
+        print("Diarization", (t3 - t2))
+        return diarize_segments
+
+    def transcribe_audio():
+        t11 = time.time()
+        transcript = model.transcribe(audio, batch_size=batch_size)
+        t22 = time.time()
+        print("Transcription time", (t22 - t11))
+        # TODO: in whisperx.asr, add task=translate only when the language is non english
+        # TODO: in whisperx.diarize, updated config.yaml path in Pipeline
+        t1 = time.time()
+        if transcript["language"] == "en":
+            result = whisperx.align(
+                transcript["segments"],
+                align_model_en,
+                align_metadata_en,
+                audio,
+                device,
+                return_char_alignments=False,
+            )
+        elif transcript["language"] == "es":
+            result = whisperx.align(
+                transcript["segments"],
+                align_model_es,
+                align_metadata_es,
+                audio,
+                device,
+                return_char_alignments=False,
+            )
+        else:
+            model_a, metadata = whisperx.load_align_model(
+                language_code=transcript["language"], device=device
+            )
+            result = whisperx.align(
+                transcript["segments"],
+                model_a,
+                metadata,
+                audio,
+                device,
+                return_char_alignments=False,
+            )
+        t2 = time.time()
+        print("Alignment time", t2 - t1)
+        return result
+
+    def adjust_speaker_labels(speaker_segments):
+        replace_speaker = {}
+        combined_speaker_data = {}
+        for idx, segment in enumerate(speaker_segments):
+            if type(segment) == str or not (segment.get("speaker")):
+                print("Segment", segment)
+                continue
+
+            # Find the longest consecutive segments of a speaker
+            if idx == 0:
+                current_speaker = segment.get("speaker")
+                current_start = segment.get("start")
+                current_end = segment.get("end")
+            elif current_speaker == segment.get("speaker"):
+                current_end = segment.get("end")
+            else:
+                current_speaker = segment.get("speaker")
+                current_start = segment.get("start")
+                current_end = segment.get("end")
+            # replace the saved segment if the current segment length is longer than the saved one
+            if current_speaker and current_speaker in combined_speaker_data:
+                if (current_end - current_start) > (
+                    combined_speaker_data.get(current_speaker, {}).get("end", 0)
+                    - combined_speaker_data.get(current_speaker, {}).get("start", 0)
+                ):
+                    combined_speaker_data[current_speaker] = {
+                        "start": current_start,
+                        "end": current_end,
+                    }
+            else:
+                combined_speaker_data[current_speaker] = {
+                    "start": current_start,
+                    "end": current_end,
+                }
+
+        for speaker_label, metadata in combined_speaker_data.items():
+            output_file = "temp_" + speaker_label + str(int(time.time())) + ".wav"
+            start_time = int(metadata.get("start") * 1000)
+            end_time = int(metadata.get("end") * 1000)
+            cut_wav(audio_path, start_time, end_time, output_file)
+            existing_speakers = local_cache.get(unique_key, {}).get("existing_speakers")
+            unique_speakers = local_cache.get(unique_key, {}).get("unique_speakers")
+            if len(existing_speakers):
+                match_found = False
+                for speaker in existing_speakers:
+                    if is_identical_speaker(
+                        output_file, speaker.get("speaker_audio_file"), threshold
+                    ):
+                        if speaker_label not in replace_speaker:
+                            replace_speaker[speaker_label] = speaker.get("speaker_name")
+                            match_found = True
+                            os.remove(output_file)
+                            break
+                    else:
+                        print(
+                            "non identical",
+                            output_file,
+                            speaker.get("speaker_audio_file"),
+                        )
+                else:
+                    if not match_found:
+                        speaker_name = f"speaker__{len(unique_speakers)}"
+                        print("Match Not found block", speaker_name)
+                        existing_speakers.append(
+                            {
+                                "speaker_id": len(existing_speakers) + 1,
+                                "speaker_name": speaker_name,
+                                "speaker_audio_file": output_file,
+                            }
+                        )
+                        print("Unique Speakers", unique_speakers)
+                        replace_speaker[speaker_label] = speaker_name
+                        unique_speakers.add(speaker_name)
+            else:
+                speaker_name = f"speaker__{len(unique_speakers)}"
+                existing_speakers.append(
+                    {
+                        "speaker_id": len(existing_speakers) + 1,
+                        "speaker_name": speaker_name,
+                        "speaker_audio_file": output_file,
+                    }
+                )
+                print("Unique Speakers ELSE BLOCK", unique_speakers)
+                replace_speaker[speaker_label] = speaker_name
+                unique_speakers.add(speaker_name)
+
+        if replace_speaker:
+            for script in speaker_segments:
+                script.pop("words", "")
+                script["speaker"] = replace_speaker.get(script.get("speaker"))
+        print("speaker_segments", speaker_segments)
+        return speaker_segments
+
+    try:
+        if diarize:
+            delay_diarize = dask.delayed(diarize_audio)()
+            delay_transcribe = dask.delayed(transcribe_audio)()
+            diarize_segments, transcribed_segments = dask.compute(
+                delay_diarize, delay_transcribe
+            )
+            t11 = time.time()
+            dialogues = whisperx.assign_word_speakers(
+                diarize_segments, transcribed_segments
+            )
+            t22 = time.time()
+            print("Assign word time", (t22 - t11))
+            speaker_segments = dialogues["segments"]
+            t1 = time.time()
+            result = adjust_speaker_labels(speaker_segments)
+            t2 = time.time()
+            print("Label Adjust Time", (t2 - t1))
+            os.remove(audio_path)
+            t3 = time.time()
+            print("Deleting file", (t3 - t2))
+            end_time = time.time()
+            print(
+                "######################################### total request time ",
+                (end_time - start_time),
+            )
+            return result
+        else:
+            transcribed_segments = transcribe_audio()
+            transcript = " ".join(
+                [
+                    segment.get("text")
+                    for segment in transcribed_segments.get("segments", [])
+                ]
+            )
+            os.remove(audio_path)
+            return transcript
+    except Exception as ex:
+        import traceback
+
+        traceback.print_exc()
+        return {"error": str(ex)}
--- speaker_identify_align.py
+++ speaker_identify_align.py
@@ -9,10 +9,13 @@ from pydub.silence import split_on_silence
 
 silence_threshold = -40
 
-speaker_model = nemo_asr.models.EncDecSpeakerLabelModel.from_pretrained("nvidia/speakerverification_en_titanet_large")
+speaker_model = nemo_asr.models.EncDecSpeakerLabelModel.from_pretrained(
+    "nvidia/speakerverification_en_titanet_large"
+)
+
 
 def is_identical_speaker(audio_1, audio_2):
-    return speaker_model.verify_speakers(audio_1,audio_2)
+    return speaker_model.verify_speakers(audio_1, audio_2)
 
 
 def cut_wav(input_wav, start_time, end_time, output_wav):
@@ -20,33 +23,48 @@ def cut_wav(input_wav, start_time, end_time, output_wav):
     cut_audio = audio[start_time:end_time]
     cut_audio.export(output_wav, format="wav")
 
+
 device = "cpu"
-batch_size = 16 # reduce if low on GPU mem
+batch_size = 16  # reduce if low on GPU mem
 compute_type = "int8"
 
 # device = "cuda"
-#audio_file = "/Users/I1597/Downloads/5sec.wav"
+# audio_file = "/Users/I1597/Downloads/5sec.wav"
 # batch_size = 32 # reduce if low on GPU mem
 # compute_type = "float16" # change to "int8" if low on GPU mem (may reduce accuracy)
 
 # 1. Transcribe with original whisper (batched)
-model = whisperx.load_model("base", device, compute_type=compute_type, asr_options={"beam_size":1})
+model = whisperx.load_model(
+    "base", device, compute_type=compute_type, asr_options={"beam_size": 1}
+)
 
 t1 = time.time()
+
 
 def transcribe(audio_file, diarize=False):
     audio = whisperx.load_audio(audio_file)
 
     def diarize_audio():
-        diarize_model = whisperx.DiarizationPipeline(device=device, use_auth_token="")
+        diarize_model = whisperx.DiarizationPipeline(
+            device=device, use_auth_token="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"
+        )
         diarize_segments = diarize_model(audio)
         print("Diarization", diarize_segments)
         return diarize_segments
 
     def transcribe_audio():
         transcript = model.transcribe(audio, batch_size=batch_size, task="translate")
-        model_a, metadata = whisperx.load_align_model(language_code=transcript["language"], device=device)
-        result = whisperx.align(transcript["segments"], model_a, metadata, audio, device, return_char_alignments=False)
+        model_a, metadata = whisperx.load_align_model(
+            language_code=transcript["language"], device=device
+        )
+        result = whisperx.align(
+            transcript["segments"],
+            model_a,
+            metadata,
+            audio,
+            device,
+            return_char_alignments=False,
+        )
         print("SEGMENTATION", result)
         return result
 
@@ -65,7 +83,10 @@ def transcribe(audio_file, diarize=False):
                     if not word.get("start") or not word.get("end"):
                         sentence += " " + word.get("word")
                         continue
-                    if word.get("start") >= segment.start and word.get("end") <= segment.end:
+                    if (
+                        word.get("start") >= segment.start
+                        and word.get("end") <= segment.end
+                    ):
                         sentence += " " + word.get("word")
                     else:
                         index -= 1
@@ -80,24 +101,32 @@ def transcribe(audio_file, diarize=False):
                 abc.append(temp)
         return abc
 
-
     try:
         if diarize:
             delay_diarize = dask.delayed(diarize_audio)()
             delay_transcribe = dask.delayed(transcribe_audio)()
-            diarize_segments, transcribed_segments = dask.compute(delay_diarize, delay_transcribe)
-            speaker_segments = assign_word_speakers(diarize_segments, transcribed_segments)
+            diarize_segments, transcribed_segments = dask.compute(
+                delay_diarize, delay_transcribe
+            )
+            speaker_segments = assign_word_speakers(
+                diarize_segments, transcribed_segments
+            )
             # speaker_segments = dialogues["segments"]
             # for segment in speaker_segments:
             #     segment.pop("words")
             #     # print(f"{segment.get('speaker')}({int(segment.get('start')//60)}:{round(segment.get('start')%60, 2)} - {int(segment.get('end')//60)}:{round(segment.get('end')%60, 2)}): {segment.get('text')}")
             t2 = time.time()
-            print("Total Time", (t2-t1))
+            print("Total Time", (t2 - t1))
             print("Transcript", speaker_segments)
             return speaker_segments
         else:
             transcribed_segments = transcribe_audio()
-            transcript = " ".join([segment.get("text") for segment in transcribed_segments.get("segments", [])])
+            transcript = " ".join(
+                [
+                    segment.get("text")
+                    for segment in transcribed_segments.get("segments", [])
+                ]
+            )
             print("Transcript", transcript)
             return transcript
     except Exception as ex:
@@ -107,10 +136,10 @@ def transcribe(audio_file, diarize=False):
 existing_speakers = []
 unique_speakers = set()
 audio_files = []
-for i in range(113):
-    audio_files.append(f"/Users/I1597/Downloads/amit_v/{i}.wav")
+for i in range(33):
+    audio_files.append(f"/Users/I1597/Downloads/mamoon_sandeep/{i}.wav")
 
-audio_files = ["/Users/I1597/Downloads/amit_v/20.wav"]
+# audio_files = ["/Users/I1597/Downloads/amit_v/20.wav"]
 print(audio_files)
 final_transcript = []
 for audio_file in audio_files:
@@ -129,17 +158,16 @@ for audio_file in audio_files:
     # audio_ = AudioSegment.from_wav(audio_file)
     # audio_segments = split_on_silence(audio_, silence_thresh=silence_threshold, seek_step=500, min_silence_len=500, keep_silence=200)
     # merged_audio = AudioSegment.empty()
-    
+
     # for seg in audio_segments:
     #     merged_audio += seg
-    
+
     # merged_audio.export(audio_file, format="wav")
     # s2 = time.time()
     # print("Silence remove time", (s2-s1))
     """
     End: Trying remove blanks from audio
     """
-
 
     print("Audio File", audio_file)
     started = time.time()
@@ -151,28 +179,31 @@ for audio_file in audio_files:
             print("Segment", segment)
             continue
         if idx == 0:
-            current_speaker = segment.get('speaker')
-            current_start = segment.get('start')
-            current_end = segment.get('end')
-        elif current_speaker == segment.get('speaker'):
-            current_end = segment.get('end')
+            current_speaker = segment.get("speaker")
+            current_start = segment.get("start")
+            current_end = segment.get("end")
+        elif current_speaker == segment.get("speaker"):
+            current_end = segment.get("end")
         else:
-            current_speaker = segment.get('speaker')
-            current_start = segment.get('start')
-            current_end = segment.get('end')
+            current_speaker = segment.get("speaker")
+            current_start = segment.get("start")
+            current_end = segment.get("end")
 
         if current_speaker and current_speaker in combined_speaker_data:
-            if (current_end-current_start) > (combined_speaker_data.get(current_speaker, {}).get("end", 0) - combined_speaker_data.get(current_speaker, {}).get("start", 0)):
+            if (current_end - current_start) > (
+                combined_speaker_data.get(current_speaker, {}).get("end", 0)
+                - combined_speaker_data.get(current_speaker, {}).get("start", 0)
+            ):
                 combined_speaker_data[current_speaker] = {
-                        "start": current_start,
-                        "end": current_end,
-                    }
+                    "start": current_start,
+                    "end": current_end,
+                }
         else:
             combined_speaker_data[current_speaker] = {
-                        "start": current_start,
-                        "end": current_end,
-                    }
-    
+                "start": current_start,
+                "end": current_end,
+            }
+
     print("Combined Speaker", combined_speaker_data)
     for speaker_label, metadata in combined_speaker_data.items():
         output_file = "temp_" + speaker_label + str(int(time.time())) + ".wav"
@@ -182,8 +213,10 @@ for audio_file in audio_files:
         if len(existing_speakers):
             match_found = False
             for speaker in existing_speakers:
-                if (is_identical_speaker(output_file, speaker.get("speaker_voice"))):
-                    print(f"Current Chunk's {segment.get('speaker')} is same as previous chunk's {speaker.get('speaker_name')}")
+                if is_identical_speaker(output_file, speaker.get("speaker_voice")):
+                    print(
+                        f"Current Chunk's {segment.get('speaker')} is same as previous chunk's {speaker.get('speaker_name')}"
+                    )
                     print("Identical ", output_file, speaker.get("speaker_voice"))
                     if speaker_label not in replace_speaker:
                         replace_speaker[speaker_label] = speaker.get("speaker_name")
@@ -199,7 +232,7 @@ for audio_file in audio_files:
                         {
                             "speaker_id": len(existing_speakers) + 1,
                             "speaker_name": speaker_name,
-                            "speaker_voice": output_file
+                            "speaker_voice": output_file,
                         }
                     )
                     print("Unique Speakers", unique_speakers)
@@ -211,69 +244,68 @@ for audio_file in audio_files:
                 {
                     "speaker_id": len(existing_speakers) + 1,
                     "speaker_name": speaker_name,
-                    "speaker_voice": output_file
+                    "speaker_voice": output_file,
                 }
             )
             print("Unique Speakers ELSE BLOCK", unique_speakers)
             replace_speaker[speaker_label] = speaker_name
             unique_speakers.add(speaker_name)
 
+    #         if len(existing_speakers):
+    #             if not(segment.get("speaker")):
+    #                 continue
+    #             print("**********", segment.get("speaker"))
+    #             output_file = "temp_" + segment.get("speaker") + str(int(time.time())) + ".wav"
+    #             start_time = int(segment.get("start") * 1000)
+    #             end_time = (segment.get("end") * 1000)
+    #             print (audio_file, start_time, end_time)
+    #             cut_wav(audio_file, start_time, end_time, output_file)
+    #             match_found = False
+    #             for speaker in existing_speakers:
+    #                 if (is_identical_speaker(output_file, speaker.get("speaker_voice"))):
+    #                     print(f"Current Chunk's {segment.get('speaker')} is same as previous chunk's {speaker.get('speaker_name')}")
+    #                     print("Identical ", output_file, speaker.get("speaker_voice"))
+    #                     if segment["speaker"] not in replace_speaker:
+    #                         replace_speaker[segment["speaker"]] = speaker.get("speaker_name")
+    #                     # segment["speaker"] = speaker.get("speaker_name")
+    #                     match_found = True
+    #                     break
+    #                 else:
+    #                     print("non identical", output_file, speaker.get("speaker_voice"))
+    #             else:
+    #                 if not match_found:
+    #                     speaker_name = f"speaker__{len(unique_speakers)}"
+    #                     print("Match block", speaker_name)
+    #                     existing_speakers.append(
+    #                         {
+    #                             "speaker_id": len(existing_speakers) + 1,
+    #                             "speaker_name": speaker_name,
+    #                             "speaker_voice": output_file
+    #                         }
+    #                     )
+    #                     print("Unique Speakers", unique_speakers)
+    #                     segment["speaker"] = speaker_name
+    #                     unique_speakers.add(speaker_name)
 
-#         if len(existing_speakers):
-#             if not(segment.get("speaker")):
-#                 continue
-#             print("**********", segment.get("speaker"))
-#             output_file = "temp_" + segment.get("speaker") + str(int(time.time())) + ".wav"
-#             start_time = int(segment.get("start") * 1000)
-#             end_time = (segment.get("end") * 1000)
-#             print (audio_file, start_time, end_time)
-#             cut_wav(audio_file, start_time, end_time, output_file)
-#             match_found = False
-#             for speaker in existing_speakers:
-#                 if (is_identical_speaker(output_file, speaker.get("speaker_voice"))):
-#                     print(f"Current Chunk's {segment.get('speaker')} is same as previous chunk's {speaker.get('speaker_name')}")
-#                     print("Identical ", output_file, speaker.get("speaker_voice"))
-#                     if segment["speaker"] not in replace_speaker:
-#                         replace_speaker[segment["speaker"]] = speaker.get("speaker_name")
-#                     # segment["speaker"] = speaker.get("speaker_name")
-#                     match_found = True
-#                     break
-#                 else:
-#                     print("non identical", output_file, speaker.get("speaker_voice"))
-#             else:
-#                 if not match_found:
-#                     speaker_name = f"speaker__{len(unique_speakers)}"
-#                     print("Match block", speaker_name)
-#                     existing_speakers.append(
-#                         {
-#                             "speaker_id": len(existing_speakers) + 1,
-#                             "speaker_name": speaker_name,
-#                             "speaker_voice": output_file
-#                         }
-#                     )
-#                     print("Unique Speakers", unique_speakers)
-#                     segment["speaker"] = speaker_name
-#                     unique_speakers.add(speaker_name)
+    #         else:
+    #             temp = {}
+    #             output_file = segment.get("speaker") + ".wav"
+    #             start_time = int(segment.get("start") * 1000)
+    #             end_time = int(segment.get("end") * 1000)
+    #             print (audio_file, start_time, end_time)
+    #             cut_wav(audio_file, start_time, end_time, output_file)
+    #             speaker_name = f"speaker__{len(unique_speakers)}"
+    #             print("Else block", speaker_name)
+    #             existing_speakers.append(
+    #                 {
+    #                     "speaker_id": len(existing_speakers) + 1,
+    #                     "speaker_name": speaker_name,
+    #                     "speaker_voice": output_file
 
-#         else:
-#             temp = {}
-#             output_file = segment.get("speaker") + ".wav"
-#             start_time = int(segment.get("start") * 1000)
-#             end_time = int(segment.get("end") * 1000)
-#             print (audio_file, start_time, end_time)
-#             cut_wav(audio_file, start_time, end_time, output_file)
-#             speaker_name = f"speaker__{len(unique_speakers)}"
-#             print("Else block", speaker_name)
-#             existing_speakers.append(
-#                 {
-#                     "speaker_id": len(existing_speakers) + 1,
-#                     "speaker_name": speaker_name,
-#                     "speaker_voice": output_file
-
-#                 }
-#             )
-#             segment["speaker"] = speaker_name
-#             unique_speakers.add(speaker_name)
+    #                 }
+    #             )
+    #             segment["speaker"] = speaker_name
+    #             unique_speakers.add(speaker_name)
 
     if replace_speaker:
         for script in transcript_segments:
@@ -281,11 +313,30 @@ for audio_file in audio_files:
 
     print(transcript_segments)
     ended = time.time()
-    print("Total Time", (ended-started))
-    print("*"*100)
+    print("Total Time", (ended - started))
+    print("*" * 100)
     print()
     final_transcript.append(transcript_segments)
 
 
-print("##"*50)
+print("##" * 50)
 print(final_transcript)
+
+
+"""
+
+from pydub import AudioSegment
+audio = AudioSegment.from_wav("amit_v.wav")
+start = 1
+start = 100
+for i in range(103):
+   start = start - 100
+   print("start", start)
+   end = start + 5100
+   cut_audio = audio[start:end]
+   start = end
+   output = f"amit_v/{i}.wav"
+   cut_audio.export(output, format="wav")
+
+
+"""
